877a0ee9929962e185194df1c5463b28a6d6a3a8
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-22 16:27:18 -0500

    Install social icons + Import social icons + Add social icons.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import DP from '../images/jamals-profile.png'
 import styles from '../styles/Header.module.css'
+import { SocialIcon } from 'react-social-icons/component'
+import 'react-social-icons/facebook'
+import 'react-social-icons/twitter'
+import 'react-social-icons/linkedin'
+import 'react-social-icons/discord'
 
 const Header = () => {
   return(
@@ -10,6 +15,10 @@ const Header = () => {
     <h1>Jamal Sadique-Donkor</h1>
     <p>A Beginner Web Developer at the PAD Forum.</p>
     {/* Social links go here */}
+    <SocialIcon url="https://www.facebook.com/people/Kwesi-SD/    pfbid0w7kmNKcEjssJumno41EMG7sE894XLFUrtjS2T33jdSADkFuEFb8PbAeQi1hHEkb2l/" />
+    <SocialIcon url="https://twitter.com/_notJamal/" />
+    <SocialIcon url="https://linkedin.com/in/jamalsd" />
+    <SocialIcon url="https://discord.com/users/508709539575758874" />
   </header>
   );
 };
