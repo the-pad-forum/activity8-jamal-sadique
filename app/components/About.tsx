561b2b06ad2237351e8721a4f6cf20c1d31bfb3b
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-22 16:08:49 -0500

    Import Social Icons +CSS: fix header background url() + Add explainer video.

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import styles from '../styles/About.module.css';
-
+import ExplainerVideo from './ExplainerVideo';
+// import explainerVid from 'https://127.0.0.1/file-name';
 const About = () => {
   return (
     <section className={styles.about}>
       <h2>About</h2>
-      <div>{/* Explainer video goes here */}</div>
+      <div>
+        <ExplainerVideo />
+        
+      </div>
       <p>{/* About text goes here */}</p>
     </section>
   );
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import DP from '../images/jamals-profile.png'
 import styles from '../styles/Header.module.css'
+import { SocialIcon } from 'react-social-icons/component'
+import 'react-social-icons/facebook'
+import 'react-social-icons/twitter'
+import 'react-social-icons/linkedin'
+import 'react-social-icons/discord'
 
 const Header = () => {
   return(
-    
   <header className={styles.header}>
     <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
     <h1>Jamal Sadique-Donkor</h1>
-    <p>A Beginner Web Developer at the PAD Forum</p>
+    <p>A Beginner Web Developer at the PAD Forum.</p>
     {/* Social links go here */}
+    <SocialIcon url="https://www.facebook.com/people/Kwesi-SD/pfbid0w7kmNKcEjssJumno41EMG7sE894XLFUrtjS2T33jdSADkFuEFb8PbAeQi1hHEkb2l/" />
+  <SocialIcon url="https://twitter.com/_notJamal/" />
+  <SocialIcon url="https://linkedin.com/in/jamalsd" />
+  <SocialIcon url="https://discord.com/users/508709539575758874" />
   </header>
   );
 };
