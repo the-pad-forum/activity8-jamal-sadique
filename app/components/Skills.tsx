ef9b75f72198554e4c428703796d64a447aed8dd
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-22 23:16:41 -0500

    Style "Skills" section.

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -3,8 +3,48 @@ import styles from '../styles/Skills.module.css';
 const Skills = () => {
   return (
     <section className={styles.skills}>
-      <h2>Skills</h2>
-      {/* Skill contents go here */}
+      <h2 className={styles.h2}>Skills</h2>
+      <div className={styles.skill}>
+        <span className={styles.skillName}>CSS</span>
+        <div className={styles.progressBar}>
+          <div className={styles.filled1}></div>
+        </div>
+        <span className={styles.skillPercent}>70%</span>
+      </div>
+
+      <div className={styles.skill}>
+        <span className={styles.skillName}>JavaScript</span>
+        <div className={styles.progressBar}>
+          <div className={styles.filled2}></div>
+        </div>
+        <span className={styles.skillPercent}>40%</span>
+      </div>
+
+      <div className={styles.skill}>
+        <span className={styles.skillName}>HTML</span>
+        <div className={styles.progressBar}>
+          <div className={styles.filled3}></div>
+        </div>
+        <span className={styles.skillPercent}>80%</span>
+      </div>
+
+      <div className={styles.skill}>
+        <span className={styles.skillName}>C++</span>
+        <div className={styles.progressBar}>
+          <div className={styles.filled4}></div>
+        </div>
+        <span className={styles.skillPercent}>60%</span>
+      </div>
+
+      
+      <div className={styles.skill}>
+        <span className={styles.skillName}>Communication</span>
+        <div className={styles.progressBar}>
+          <div className={styles.filled5}></div>
+        </div>
+        <span className={styles.skillPercent}>77%</span>
+      </div>
+      
     </section>
   );
 };
