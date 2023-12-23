baa55321f72acc9e4440bb37a9aba2678b53bac6
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-22 22:38:19 -0500

    Add styles to the "about" section.

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,9 +5,14 @@ import ExplainerVideo from './ExplainerVideo';
 const About = () => {
   return (
     <section className={styles.about}>
-      <h2>About</h2>
-      <div>
-        <ExplainerVideo/>
+      <h2 className={styles.h2}>ABOUT</h2>
+        <div className={styles.aboutVideo}><ExplainerVideo /></div>
+        <div className={styles.aboutText}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rerum, quas provident dicta dignissimos modi quod hic, autem suscipit itaque distinctio mollitia earum porro accusamus assumenda, maxime qui beatae voluptas! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum accusantium dolores beatae. Officiis iste itaque optio numquam eum reiciendis iure. Cupiditate cum maxime illo saepe magnam consequuntur totam laboriosam reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem obcaecati aliquam, voluptates nulla voluptas ducimus eveniet. Temporibus libero aliquid natus praesentium modi beatae nostrum, rerum accusamus tempore ducimus. Quibusdam, illum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cum, enim consectetur rem blanditiis itaque eum. Eaque quasi laudantium labore corrupti porro cumque corporis quidem! Distinctio, commodi! Labore, asperiores libero.</p>
+          <br></br>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam minima fugiat illum corporis repellendus voluptatem alias veritatis sint laudantium doloribus, facilis, dolor sequi ipsam harum temporibus! Nam recusandae quod tenetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt et similique eligendi accusantium odit delectus esse praesentium porro sint quaerat laborum est officia impedit repellat perspiciatis sapiente sit, nemo labore! 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo alias, nam beatae quo odit magnam ducimus temporibus laborum incidunt ad voluptatem minus, id similique tempore natus ipsam, cum unde rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem rerum qui laborum illo quia culpa. Distinctio ipsa consequatur doloremque labore aut neque recusandae corrupti, voluptates quam odit optio necessitatibus pariatur.
+        </p>
       </div>
       <p>{/* About text goes here */}</p>
     </section>
