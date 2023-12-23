0009c89edcc0a18127ebb064341d418e403bb448
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-22 19:46:05 -0500

    Header: re-style the header.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import DP from '../images/jamals-profile.png'
+import DP from '../images/my-photo.jpg'
 import styles from '../styles/Header.module.css'
 import { SocialIcon } from 'react-social-icons/component'
 import 'react-social-icons/facebook'
@@ -11,14 +11,18 @@ const Header = () => {
   return(
     
   <header className={styles.header}>
-    <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
-    <h1>Jamal Sadique-Donkor</h1>
-    <p>A Beginner Web Developer at the PAD Forum.</p>
-    {/* Social links go here */}
-    <SocialIcon url="https://www.facebook.com/people/Kwesi-SD/    pfbid0w7kmNKcEjssJumno41EMG7sE894XLFUrtjS2T33jdSADkFuEFb8PbAeQi1hHEkb2l/" />
-    <SocialIcon url="https://twitter.com/_notJamal/" />
-    <SocialIcon url="https://linkedin.com/in/jamalsd" />
-    <SocialIcon url="https://discord.com/users/508709539575758874" />
+    <div className={styles.overlay}>
+      <Image className={styles.dp} src={DP} width="130" height="130" alt="" />
+      <h1 className={styles.h1}>Jamal Sadique</h1>
+      <p className={styles.p}>A Beginner Web Developer at The PAD Forum!</p>
+      {/* Social links go here */}
+      <div className={styles.socialIcons}>
+        <SocialIcon className={styles.i} url="https://www.facebook.com/people/Kwesi-SD/    pfbid0w7kmNKcEjssJumno41EMG7sE894XLFUrtjS2T33jdSADkFuEFb8PbAeQi1hHEkb2l/" />
+        <SocialIcon className={styles.i} url="https://twitter.com/_notJamal/" />
+        <SocialIcon className={styles.i} url="https://linkedin.com/in/jamalsd" />
+        <SocialIcon className={styles.i} url="https://discord.com/users/508709539575758874" />
+      </div>
+    </div>
   </header>
   );
 };
