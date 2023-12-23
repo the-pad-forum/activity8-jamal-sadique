ca1d41992fb02ada2cdca45d987457d9a9905e9f
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-23 02:40:29 -0500

    refactor: Add percentage selectors.

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
       <div className={styles.skill}>
         <span className={styles.skillName}>CSS</span>
         <div className={styles.progressBar}>
-          <div className={styles.filled1}></div>
+          <div className={`${styles.filled} ${styles.p70}`}></div>
         </div>
         <span className={styles.skillPercent}>70%</span>
       </div>
@@ -15,7 +15,7 @@ const Skills = () => {
       <div className={styles.skill}>
         <span className={styles.skillName}>JavaScript</span>
         <div className={styles.progressBar}>
-          <div className={styles.filled2}></div>
+          <div className={`${styles.filled} ${styles.p40}`}></div>
         </div>
         <span className={styles.skillPercent}>40%</span>
       </div>
@@ -23,7 +23,7 @@ const Skills = () => {
       <div className={styles.skill}>
         <span className={styles.skillName}>HTML</span>
         <div className={styles.progressBar}>
-          <div className={styles.filled3}></div>
+          <div className={`${styles.filled} ${styles.p80}`}></div>
         </div>
         <span className={styles.skillPercent}>80%</span>
       </div>
@@ -31,7 +31,7 @@ const Skills = () => {
       <div className={styles.skill}>
         <span className={styles.skillName}>C++</span>
         <div className={styles.progressBar}>
-          <div className={styles.filled4}></div>
+          <div className={`${styles.filled} ${styles.p60}`}></div>
         </div>
         <span className={styles.skillPercent}>60%</span>
       </div>
@@ -40,11 +40,10 @@ const Skills = () => {
       <div className={styles.skill}>
         <span className={styles.skillName}>Communication</span>
         <div className={styles.progressBar}>
-          <div className={styles.filled5}></div>
+          <div className={`${styles.filled} ${styles.p77}`}></div>
         </div>
         <span className={styles.skillPercent}>77%</span>
       </div>
-      
     </section>
   );
 };
